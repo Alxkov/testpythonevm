09e2c331691f56f8afff7fd3ad7a65558869c401
PrinceOfRussia <sas-lyt@ya.ru>
2019-09-13 02:32:17 +0300

    commit 3

--- test.py
+++ test.py
@@ -1,59 +1,111 @@
 #!/usr/bin/python
 
 import csv
+import itertools
+import sys
+
+from random import randrange, sample
+from random import *
+
+def random_insert(lst, item):
+    lst.insert(randrange(len(lst)+1), item)
+
 def getlogvalue(opnd, row):
-    if(opnd == 't'):
-       return 't'
-    if(opnd == 'f'):
-       return 'f'
-    if(row[opnd] == 't'):
-       return 't'
-    if(row[opnd] == 'f'):
-       return 'f'
+    if(opnd == '1'):
+       return '1'
+    if(opnd == '0'):
+       return '0'
+    if(row[opnd] == '1'):
+       return '1'
+    if(row[opnd] == '0'):
+       return '0'
 
 def neg(opnd):
-    if(opnd == 't'):
-       return 'f'
-    if(opnd == 'f'):
-       return 't'
+    if(opnd == '1'):
+       return '0'
+    if(opnd == '0'):
+       return '1'
 
 def con(opnd1, opnd2):
-    if(opnd1 == 't' and opnd2 == 't'):
-       return 't'
+    if(opnd1 == '1' and opnd2 == '1'):
+       return '1'
     else:
-       return 'f'   
+       return '0'   
 
 def dis(opnd1, opnd2):
-    if(opnd1 == 'f' and opnd2 == 'f'):
-       return 'f'
+    if(opnd1 == '0' and opnd2 == '0'):
+       return '0'
     else:
-       return 't'  
+       return '1'  
 
 def checkcorrect(opndList, opsList, table):
     #return false if doesn't correspond with table
+    #print("before")
+    #print(opndList)
     for row in table:
-        while len(opndList) > 1:
-            curOpr = opsList.pop()
-            curOpnd1 = opndList.pop()
-            if(curOpr == 'n'):
-                opndList.append(neg(getlogvalue(curOpnd1, row)))
-            if(curOpr == 'c'):
-                curOpnd2 = opndList.pop()
-                opndList.append(con(getlogvalue(opnd1),getlogvalue(opnd2)))    
+        tmp1 = list(opndList)
+        tmp2 = list(opsList)
+        while len(tmp1) > 1:
+            curOpr = tmp2.pop()
+            curOpnd1 = tmp1.pop()
+            if(curOpr == 'not'):
+                tmp1.append(neg(getlogvalue(curOpnd1, row)))
+            if(curOpr == 'and'):
+                curOpnd2 = tmp1.pop()
+                tmp1.append(con(getlogvalue(curOpnd1, row),getlogvalue(curOpnd2, row)))    
             if(curOpr == 'd'):
-                curOpnd2 = opndList.pop()
-                opndList.append(con(getlogvalue(opnd1),getlogvalue(opnd2))) 
-
+                curOpnd2 = tmp1.pop()
+                tmp1.append(con(getlogvalue(curOpnd1, row),getlogvalue(curOpnd2, row))) 
         
+        if(getlogvalue(tmp1[0], row) != row[len(row)-1]):
+           #print("after")
+           #print(opndList)
+           return False
+        #print("opndList:")
+        #print(opndList)    
+    #print("after")
+   # print(opndList) 
     return True
 
+	
 with open('test.csv', 'r') as fp:
     reader = csv.reader(fp, delimiter=',', quotechar='"')
     # next(reader, None)  # skip the headers
     data_read = [row for row in reader]
+data_list = []
+for i in data_read:
+    data_list.append(list(i))
 
-print(data_read)
-nOpnd = len(data_read[0])
+#print(data_read)
+nOpnd = len(data_read[0]) - 1
+x = range(nOpnd)
+
+curnOpnd = 4
+opndListList = [list(p) for p in itertools.product(x, repeat= curnOpnd)]
+nFound = 0
+N = sys.argv[1]
+while nFound < int(N) :
+  for i in opndListList:
+      if(nFound >= int(N)):
+         break
+      y = {'and','or'}
+      oprListList = [list(t) for t in itertools.product(y, repeat=curnOpnd - 1)]
+      for j in oprListList:
+         if(random() > 0.5):
+           random_insert(j,'not')
+         if(random() > 0.3):
+           random_insert(j,'not')
+         if(random() > 0.1):
+           random_insert(j,'not')
+         if checkcorrect(i,j,data_list) == True:
+           nFound = nFound + 1
+           print("expr: " + repr(nFound))
+           print(i)
+           print(j) 
+           if(nFound >= int(N)):
+              break
+    #print(oprListList)
+print(nFound)
 
 
 
